bab1934d0cd09a9db25a946524a80a7985f1bda1
ratataca <gywls18@koreatech.ac.kr>
2022-04-29 16:44:04 +0900

    add

--- Photovle/settings.py
+++ Photovle/settings.py
@@ -64,6 +64,14 @@ INSTALLED_APPS = [
 
 ]
 
+# kakao client key
+SOCIAL_OUTH_CONFIG = {
+    'KAKAO_REST_API_KEY': secrets['KAKAO_REST_API_KEY'],
+    'KAKAO_REDIRECT_URI': secrets['KAKAO_REDIRECT_URI'],
+    'KAKAO_SECRET_KEY': secrets['KAKAO_SECRET_KEY'],
+}
+
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 
@@ -91,13 +99,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ],
     'PAGE_SIZE': 10
-}
-
-# kakao client key
-SOCIAL_OUTH_CONFIG = {
-    'KAKAO_REST_API_KEY': secrets['KAKAO_REST_API_KEY'],
-    'KAKAO_REDIRECT_URI': secrets['KAKAO_REDIRECT_URI'],
-    'KAKAO_SECRET_KEY': secrets['KAKAO_SECRET_KEY'],
 }
 
 MIDDLEWARE = [
@@ -145,6 +146,7 @@ WSGI_APPLICATION = 'Photovle.wsgi.application'
 
 DATABASES = {
     'default': {
+<<<<<<< HEAD
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.mysql',
@@ -153,6 +155,19 @@ DATABASES = {
         # 'PASSWORD': '1234',
         # 'HOST': 'localhost',
         # 'PORT': ''
+=======
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'photovle',
+        'USER': 'root', 
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS':{
+            'charset':'utf8mb4'
+        }
+>>>>>>> origin/main
     }
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
@@ -208,6 +223,6 @@ AUTH_USER_MODEL = 'main.User'
 
 # login session
 ACCOUNT_SESSION_REMEMBER = True     # 브라우저를 닫아도 로그인 유지
-SESSION_COOKIE_AGE = 72000       # 쿠키 저장 유효시간(단위 sec)
+SESSION_COOKIE_AGE = 7200       # 쿠키 저장 유효시간(단위 sec)
 
 
